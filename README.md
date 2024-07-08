d687cebcf5dd4ee556c43df165501cd6c9f73873
Mateus Hellmann <mateus54099606@aluno.ms.senac.br>
2024-07-08 15:13:26 -0400

    removerItens

--- README.md
+++
@@ -1,1 +0,0 @@
-# Exercicios-lista-1
--- ex39.py
+++
@@ -1,4 +0,0 @@
-valor=float(input("Digite o valor da hora de um trabalho: "))
-dias=int(input("Digite a quantidade de dias trabalhados: "))
-total=(valor*dias)*1.1
-print(f"A quantidade que deverá ser paga será: {total}")
